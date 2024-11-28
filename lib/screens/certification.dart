768651307205b2318959b2deb0919ca20b4d9713
Jae-ik <jakeforest2749@gmail.com>
2024-11-28 17:59:56 +0900

    fix ndkVersion

--- lib/screens/certification.dart
+++ lib/screens/certification.dart
@@ -26,7 +26,11 @@ class Certification extends StatelessWidget {
       data: CertificationData(
         pg: 'inicis_unified',
         merchantUid: 'mid_${DateTime.now().millisecondsSinceEpoch}',
-        mRedirectUrl: 'http://detectchangingwebview/iamport/f',
+        // mRedirectUrl ? 리디렉션 될 URL. (https://developers.portone.io/opi/ko/extra/identity-verification/v1/all/1?v=v1)
+        //                본인인증을 완료하면 다시 리디렉션될 주소... 라고 하지만 아무 값이라도 다됨.
+        mRedirectUrl: 'http://example.com',
+        // mRedirectUrl: 'http://10.0.2.2:8080/api/v1/certification/verify',
+        // mRedirectUrl: 'http://detectchangingwebview/iamport/f',
       ),
       callback: (Map<String, String> result) {
         Navigator.pushReplacementNamed(
